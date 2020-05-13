dc039925d6ad9e609efa939fee4babcbd07ff82b
Vanessa Aguilar <vanessa.aguilar@ecosia.org>
2020-05-13 15:26:23 +0200

    remove boilerplates

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,19 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        sustainable-growing
+        Sustainable Growing 🌱
       </h1>
       <h2 class="subtitle">
-        helping you make more sustainable choices
+        Helping you make more sustainable gardening choices
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
-  }
 }
 </script>
 
@@ -64,9 +43,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
